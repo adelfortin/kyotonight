9961819ec7dab0115c6228b4d88e59c50d9d0e59
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-07-28 09:37:10 +0200

    changement des couleurs dans la barre lua

--- lua/lualine/themes/kyotonight.lua
+++ lua/lualine/themes/kyotonight.lua
@@ -3,47 +3,58 @@ local colors = require('kyotonight.colors')
 local kyotonight = {}
 
 kyotonight.normal = {
-  a = { bg = colors.blue0[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.blue0[1] },
-  c = { bg = colors.black0[1], fg = colors.fg[1] },
-  x = { bg = colors.black0[1], fg = colors.blue0[1] },
+        a = {fg = c.blue3, bg = c.blue1, gui = "bold"},
+        b = {fg = c.blue3, bg = c.blue2},
+        c = {fg = c.blue1, bg = c.blue3},
+        x = {fg = c.blue1, bg = c.blue3},
+        y = {fg = c.blue3, bg = c.blue2, gui = "bold"},
+        z = {fg = c.blue3, bg = c.blue1, gui = "bold"}
 }
 
 kyotonight.insert = {
-  a = { bg = colors.green[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.green[1] },
-  x = { bg = colors.black0[1], fg = colors.green[1] },
+        a = {fg = c.green3, bg = c.green1, gui = "bold"},
+        b = {fg = c.green3, bg = c.green2},
+        c = {fg = c.green1, bg = c.green3},
+        x = {fg = c.green1, bg = c.green3},
+        y = {fg = c.green3, bg = c.green2, gui = "bold"},
+        z = {fg = c.green3, bg = c.green1, gui = "bold"}
 }
 
 kyotonight.command = {
-  a = { bg = colors.yellow[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.yellow[1] },
-  x = { bg = colors.black0[1], fg = colors.yellow[1] },
+        a = {fg = c.purple3, bg = c.purple1, gui = "bold"},
+        b = {fg = c.purple3, bg = c.purple2},
+        c = {fg = c.purple1, bg = c.purple3},
+        x = {fg = c.purple1, bg = c.purple3}
 }
 
 kyotonight.visual = {
-  a = { bg = colors.magenta[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.magenta[1] },
-  x = { bg = colors.black0[1], fg = colors.magenta[1] },
+        a = {fg = c.yellow3, bg = c.yellow1, gui = "bold"},
+        b = {fg = c.yellow3, bg = c.yellow2},
+        c = {fg = c.yellow1, bg = c.yellow3},
+        x = {fg = c.yellow1, bg = c.yellow3},
+        y = {fg = c.yellow1, bg = c.yellow2, gui = "bold"},
+        z = {fg = c.yellow3, bg = c.yellow1, gui = "bold"}
 }
 
 kyotonight.replace = {
-  a = { bg = colors.red[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.red[1] },
-  x = { bg = colors.black0[1], fg = colors.red[1] },
+        a = {fg = c.red3, bg = c.red1, gui = "bold"},
+        b = {fg = c.red3, bg = c.red2},
+        c = {fg = c.red2, bg = c.red3},
+        x = {fg = c.red2, bg = c.red3}
 }
 
 kyotonight.terminal = {
-  a = { bg = colors.teal[1], fg = colors.black1[1] },
-  b = { bg = colors.black1[1], fg = colors.teal[1] },
-  x = { bg = colors.black0[1], fg = colors.teal[1] },
+        a = {fg = c.orange3, bg = c.orange1, gui = "bold"},
+        b = {fg = c.orange3, bg = c.orange2},
+        c = {fg = c.orange1, bg = c.orange3},
+        x = {fg = c.orange1, bg = c.orange3},
+        y = {fg = c.orange3, bg = c.orange2, gui = "bold"},
+        z = {fg = c.orange3, bg = c.orange1, gui = "bold"}
 }
 
 kyotonight.inactive = {
-  a = { bg = colors.black0[1], fg = colors.blue0[1] },
-  b = { bg = colors.black0[1], fg = colors.black1[1], gui = "bold" },
-  c = { bg = colors.black0[1], fg = colors.black1[1] },
-  x = { bg = colors.black0[1], fg = colors.black1[1] },
+        a = {fg = c.gray3, bg = c.gray1},
+        b = {fg = c.gray1, bg = c.gray3}
 }
 
 if vim.g.kyotonight_lualine_bold then
